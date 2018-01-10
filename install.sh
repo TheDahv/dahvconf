0a9c4368699ea9e5d17e18d91fb1f7fc184ff0b4
David Pierce <david.pierce@moz.com>
2018-01-10 15:50:26 -0800

    Set up custom zsh theming

--- install.sh
+++ install.sh
@@ -5,9 +5,15 @@ DAHVCONF=$(dirname $0)
 echo 'Setting up links...'
 ln -s $DAHVCONF/.zshrc
 ln -s $DAHVCONF/.vimrc
-ln -s $DAHVCONF/.oh-my-zsh $HOME/.oh-my-zsh
 ln -s $DAHVCONF/.tmux.conf $HOME/.tmux.conf
 ln -s $DAHVCONF/.tmux.powerline.conf $HOME/.tmux.powerline.conf
+
+echo 'Downloading ohmyzsh'
+pushd $DAHVCONF
+git submodule update --init
+cp ./snow.zsh-theme .oh-my-zsh/themes/snow.zsh-theme
+popd
+ln -s $DAHVCONF/.oh-my-zsh ~/.oh-my-zsh
 
 if [ $(basename $SHELL) != 'zsh' ]; then
   echo 'Setting shell to zsh...'
